12249f4e43e9d760a2e1a81b03e17f95b831510e
DESKTOP-13AG486\Christian <christian.wnr@gmail.com>
2018-01-12 14:57:49 +0100

    Created procedure

--- Data Analysis Dataset.R
+++ Data Analysis Dataset.R
@@ -2,3 +2,17 @@
 data11 = read.csv("Datasets/data_11.csv")
 data11 = read.csv("Datasets/data_14.csv")
 data02 = get(load("Datasets/data_02.RData"))
+
+#1. Data Pre-processing#
+
+#1.1 Exploratory Data Analysis#
+
+#1.2 Outlier detection#
+
+#1.3 Outlier removal#
+
+#1.3 Testing for normal distribution#
+
+#2. Dimensionality Reduction#
+
+#3. Cluster Analysis#

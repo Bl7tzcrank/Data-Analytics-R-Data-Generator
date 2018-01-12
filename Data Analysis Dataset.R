267d1bca2de267763584629f72b0b157971ce9b6
DESKTOP-13AG486\Christian <christian.wnr@gmail.com>
2018-01-12 15:19:27 +0100

    Removed mistake

--- Data Analysis Dataset.R
+++ Data Analysis Dataset.R
@@ -1,6 +1,6 @@
 ####R-Script for analysing the given dataset####
 data11 = read.csv("Datasets/data_11.csv")
-data11 = read.csv("Datasets/data_14.csv")
+data14 = read.csv("Datasets/data_14.csv")
 data02 = get(load("Datasets/data_02.RData"))
 
 #1. Data Pre-processing#
